bdc63612d6b8379fc58aeee9eff2047da3e25474
Melanie <marketing@webliners.nl>
2023-05-27 16:22:30 +0200

    slugfield

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -9,6 +10,8 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    slug = models.SlugField(null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,3 +19,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"slug": self.slug})
