64360f4242b3690b5fe95c6623710b30b28dff16
Melanie <marketing@webliners.nl>
2023-05-28 11:34:08 +0200

    titel in url

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('admin/', admin.site.urls),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path("post/<slug:slug>/", views.post_detail, name="post_detail"),
 ]
