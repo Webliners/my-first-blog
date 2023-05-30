54c8bbb0cbd8343c2a5f0847a7a2baabbc711b12
Livcamp <info@livcamp.nl>
2023-05-30 16:11:55 +0200

    added new post form

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('admin/', admin.site.urls),
+    path('new/', views.post_new, name='post_new'),
     # path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path("post/<slug:slug>/", views.post_detail, name="post_detail"),
+    path("<slug:slug>/", views.post_detail, name="post_detail"),
 ]
